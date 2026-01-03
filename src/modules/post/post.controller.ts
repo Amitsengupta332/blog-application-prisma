b984bb0b4fa360e498ed0eb9750bdd5cb4f5105f
amitsengupta <amitsengupta332@gmail.com>
2026-01-04 00:50:22 +0600

    Pagination And Sorting Helper

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
 import { PostStatus } from "../../../generated/prisma/enums";
+import paginationSortingHelper from "../../helpers/paginationSortingHelper";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -40,12 +41,16 @@ const getAllPost = async (req: Request, res: Response) => {
 
     const authorId = req.query.authorId as string | undefined;
 
-    const page = Number(req.query.page ?? 1);
-    const limit = Number(req.query.limit ?? 10);
-    const skip = (page - 1) * limit;
+    // const page = Number(req.query.page ?? 1);
+    // const limit = Number(req.query.limit ?? 10);
+    // const skip = (page - 1) * limit;
 
-    const sortBy = req.query.sortBy as string | undefined;
-    const sortOrder = req.query.sortOrder as string | undefined;
+    // const sortBy = req.query.sortBy as string | undefined;
+    // const sortOrder = req.query.sortOrder as string | undefined;
+
+    const { page, limit, skip, sortBy, sortOrder } = paginationSortingHelper(
+      req.query
+    );
 
     const result = await postService.getAllPost({
       search: searchString,
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -16,94 +16,94 @@ const createPost = async (
 };
 
 const getAllPost = async ({
-    search,
-    tags,
-    isFeatured,
-    status,
-    authorId,
-    page,
-    limit,
-    skip,
-    sortBy,
-    sortOrder
-
+  search,
+  tags,
+  isFeatured,
+  status,
+  authorId,
+  page,
+  limit,
+  skip,
+  sortBy,
+  sortOrder,
 }: {
-    search: string | undefined,
-    tags: string[] | [],
-    isFeatured: boolean | undefined,
-    status: PostStatus | undefined,
-    authorId: string | undefined,
-    page: number,
-    limit: number,
-    skip: number,
-    sortBy: string | undefined,
-    sortOrder: string | undefined
-
+  search: string | undefined;
+  tags: string[] | [];
+  isFeatured: boolean | undefined;
+  status: PostStatus | undefined;
+  authorId: string | undefined;
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string | undefined;
+  sortOrder: string | undefined;
 }) => {
-    const andConditions: PostWhereInput[] = []
+  const andConditions: PostWhereInput[] = [];
 
-    if (search) {
-        andConditions.push({
-            OR: [
-                {
-                    title: {
-                        contains: search,
-                        mode: "insensitive"
-                    }
-                },
-                {
-                    content: {
-                        contains: search,
-                        mode: "insensitive"
-                    }
-                },
-                {
-                    tags: {
-                        has: search
-                    }
-                }
-            ]
-        })
-    }
-
-    if (tags.length > 0) {
-        andConditions.push({
-            tags: {
-                hasEvery: tags as string[]
-            }
-        })
-    }
-
-    if (typeof isFeatured === 'boolean') {
-        andConditions.push({
-            isFeatured
-        })
-    }
-
-    if (status) {
-        andConditions.push({
-            status
-        })
-    }
-
-    if (authorId) {
-        andConditions.push({
-            authorId
-        })
-    }
-
-    const allPost = await prisma.post.findMany({
-        take: limit,
-        skip,
-        where: {
-            AND: andConditions
+  if (search) {
+    andConditions.push({
+      OR: [
+        {
+          title: {
+            contains: search,
+            mode: "insensitive",
+          },
         },
-        orderBy: {
-            [sortBy]: sortOrder
-        }
+        {
+          content: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+        {
+          tags: {
+            has: search,
+          },
+        },
+      ],
     });
-    return allPost;
-}
+  }
+
+  if (tags.length > 0) {
+    andConditions.push({
+      tags: {
+        hasEvery: tags as string[],
+      },
+    });
+  }
+
+  if (typeof isFeatured === "boolean") {
+    andConditions.push({
+      isFeatured,
+    });
+  }
+
+  if (status) {
+    andConditions.push({
+      status,
+    });
+  }
+
+  if (authorId) {
+    andConditions.push({
+      authorId,
+    });
+  }
+
+  const allPost = await prisma.post.findMany({
+    take: limit,
+    skip,
+    where: {
+      AND: andConditions,
+    },
+    // orderBy: {
+    //     [sortBy]: sortOrder
+    // }
+    orderBy:
+      sortBy && sortOrder ? { [sortBy]: sortOrder } : { createdAt: "desc" },
+  });
+  return allPost;
+};
 
 export const postService = {
   createPost,
