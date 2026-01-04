57648c9cc851bf1ac78361c2122670d9d4af515a
amitsengupta <amitsengupta332@gmail.com>
2026-01-04 21:57:04 +0600

    Helper Function For Sorting And Pagination

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -41,13 +41,6 @@ const getAllPost = async (req: Request, res: Response) => {
 
     const authorId = req.query.authorId as string | undefined;
 
-    // const page = Number(req.query.page ?? 1);
-    // const limit = Number(req.query.limit ?? 10);
-    // const skip = (page - 1) * limit;
-
-    // const sortBy = req.query.sortBy as string | undefined;
-    // const sortOrder = req.query.sortOrder as string | undefined;
-
     const { page, limit, skip, sortBy, sortOrder } = paginationSortingHelper(
       req.query
     );
