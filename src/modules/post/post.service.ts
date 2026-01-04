799cbd780efe7352c1e79ce38738d319c46cdfa6
amitsengupta <amitsengupta332@gmail.com>
2026-01-05 00:01:12 +0600

    Get Post By Id And Increment View Count And Transaction

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -119,12 +119,25 @@ const getAllPost = async ({
 // (id: string)
 
 const getPostById = async (postId: string) => {
-  const postData = await prisma.post.findUnique({
-    where: {
-      id: postId,
-    },
+  return await prisma.$transaction(async (tx) => {
+    await tx.post.update({
+      where: {
+        id: postId,
+      },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+    });
+    const postData = await tx.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+
+    return postData;
   });
-  return postData;
 };
 export const postService = {
   createPost,
