9f3378b43ea03b08116bd51771739aeaff10aacf
amitsengupta <amitsengupta332@gmail.com>
2026-01-05 21:40:20 +0600

    Create Comment Part 2

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,10 +1,28 @@
+import { prisma } from "../../lib/prisma";
+
 const createComment = async (payload: {
   content: string;
   authorId: string;
   postId: string;
   parentId?: string;
 }) => {
-  console.log("create comment", payload);
+  //   console.log("create comment", payload);
+  await prisma.post.findUniqueOrThrow({
+    where: {
+      id: payload.postId,
+    },
+  });
+
+  if (payload.parentId) {
+    await prisma.comment.findUniqueOrThrow({
+      where: {
+        id: payload.parentId,
+      },
+    });
+  }
+  return await prisma.comment.create({
+    data: payload,
+  });
 };
 
 export const CommentService = {
