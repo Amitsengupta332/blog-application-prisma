ab6e756cec7eec0a1dee6d03a74fa89a2968b3c9
amitsengupta <amitsengupta332@gmail.com>
2026-01-05 23:16:22 +0600

    Comment Count And Ordering

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -20,106 +20,112 @@ const createPost = async (
 };
 
 const getAllPost = async ({
-  search,
-  tags,
-  isFeatured,
-  status,
-  authorId,
-  page,
-  limit,
-  skip,
-  sortBy,
-  sortOrder,
-}: {
-  search: string | undefined;
-  tags: string[] | [];
-  isFeatured: boolean | undefined;
-  status: PostStatus | undefined;
-  authorId: string | undefined;
-  page: number;
-  limit: number;
-  skip: number;
-  sortBy: string;
-  sortOrder: string;
-}) => {
-  const andConditions: PostWhereInput[] = [];
-
-  if (search) {
-    andConditions.push({
-      OR: [
-        {
-          title: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-        {
-          content: {
-            contains: search,
-            mode: "insensitive",
-          },
-        },
-        {
-          tags: {
-            has: search,
-          },
-        },
-      ],
-    });
-  }
-
-  if (tags.length > 0) {
-    andConditions.push({
-      tags: {
-        hasEvery: tags as string[],
-      },
-    });
-  }
-
-  if (typeof isFeatured === "boolean") {
-    andConditions.push({
-      isFeatured,
-    });
-  }
-
-  if (status) {
-    andConditions.push({
-      status,
-    });
-  }
-
-  if (authorId) {
-    andConditions.push({
-      authorId,
-    });
-  }
-
-  const allPost = await prisma.post.findMany({
-    take: limit,
+    search,
+    tags,
+    isFeatured,
+    status,
+    authorId,
+    page,
+    limit,
     skip,
-    where: {
-      AND: andConditions,
-    },
-    orderBy: {
-      [sortBy]: sortOrder,
-    },
-  });
+    sortBy,
+    sortOrder
+}: {
+    search: string | undefined,
+    tags: string[] | [],
+    isFeatured: boolean | undefined,
+    status: PostStatus | undefined,
+    authorId: string | undefined,
+    page: number,
+    limit: number,
+    skip: number,
+    sortBy: string,
+    sortOrder: string
+}) => {
+    const andConditions: PostWhereInput[] = []
 
-  const total = await prisma.post.count({
-    where: {
-      AND: andConditions,
-    },
-  });
-  return {
-    data: allPost,
-    pagination: {
-      total,
-      page,
-      limit,
-      totalPages: Math.ceil(total / limit),
-    },
-  };
-};
+    if (search) {
+        andConditions.push({
+            OR: [
+                {
+                    title: {
+                        contains: search,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    content: {
+                        contains: search,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    tags: {
+                        has: search
+                    }
+                }
+            ]
+        })
+    }
+
+    if (tags.length > 0) {
+        andConditions.push({
+            tags: {
+                hasEvery: tags as string[]
+            }
+        })
+    }
+
+    if (typeof isFeatured === 'boolean') {
+        andConditions.push({
+            isFeatured
+        })
+    }
+
+    if (status) {
+        andConditions.push({
+            status
+        })
+    }
+
+    if (authorId) {
+        andConditions.push({
+            authorId
+        })
+    }
+
+    const allPost = await prisma.post.findMany({
+        take: limit,
+        skip,
+        where: {
+            AND: andConditions
+        },
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: {
+            _count: {
+                select: { comments: true }
+            }
+        }
+    });
+
+    const total = await prisma.post.count({
+        where: {
+            AND: andConditions
+        }
+    })
+    return {
+        data: allPost,
+        pagination: {
+            total,
+            page,
+            limit,
+            totalPages: Math.ceil(total / limit)
+        }
+    };
+}
+
 // (id: string)
 
 const getPostById = async (postId: string) => {
